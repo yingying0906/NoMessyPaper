92c393e63fbecf5f54851610f86afa1437d4a9b5
Law Po Ying <30721578+yingying0906@users.noreply.github.com>
2023-05-28 23:37:12 +0800

    fix net

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,11 @@
+import ServerlessHttp from "serverless-http";
+
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
-
-const PORT = process.env.PORT || 3001;
-const HOST = "0.0.0.0";
 
 const API_SERVICE_URL = "https://serpapi.com";
 
@@ -16,12 +15,9 @@ app.use(
     target: API_SERVICE_URL,
     changeOrigin: true,
     pathRewrite: {
-      [`^/paper`]: "",
+      "^/paper": "",
     },
   })
 );
 
-// Start Proxy
-app.listen(PORT, HOST, () => {
-  console.log(`Starting Proxy at ${HOST}:${PORT}`);
-});
+module.exports.handler = ServerlessHttp(app);
