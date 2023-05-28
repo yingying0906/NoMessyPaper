285df5e9d8f827dbc00dd18eeaea300cad508dea
Law Po Ying <30721578+yingying0906@users.noreply.github.com>
2023-05-28 21:48:27 +0800

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+console.log("server.js running");
 const API_SERVICE_URL =
   "https://serpapi.com/searches/bb6e2fcc452dcb5b/6464ec487f8361e3061a9c72.json";
 
